a4e7ab2c63a5f1bb051d74dd3b3e47540d7388d3
Erick0105 <erickaxs0105@gmail.com>
2024-10-21 11:18:01 -0300

    criação de nova senha

--- arquivos-sql/ultima-aula.sql
+++ arquivos-sql/ultima-aula.sql
@@ -17,6 +17,19 @@ SELECT cod_ven, COUNT(NUM_PEDIDO) from PEDIDO group by COD_VEN order by 2;
 SELECT NOME_VEN, SALARIO_FIXO from VENDEDOR
 where SALARIO_FIXO in (SELECT max(SALARIO_FIXO) from VENDEDOR);
 
+
 --Funções caracteres
 
 SELECT NOME_CLIE, UPPER(NOME_CLIE) "MAIUSCULO", lower(NOME_CLIE) "minusculo", INITCAP(NOME_CLIE) "1aº MAIUSCULO", LENGTH(NOME_CLIE) "Conta Caracter" from CLIENTE;
+
+--Exibir os dados do cliente "Flavio"
+
+SELECT * from CLIENTE where UPPER(NOME_CLIE) = 'FLAVIO';
+SELECT * from CLIENTE where lower(NOME_CLIE) = 'flavio';
+SELECT * from CLIENTE where INITCAP(NOME_CLIE) = 'Flavio';
+
+--SUBSTR(coluna, inicio da leitura, qtd de caracteres) 
+SELECT NOME_CLIE, SUBSTR(NOME_CLIE,2,3) from CLIENTE;
+
+--nova senha
+SELECT SUBSTR(CEP,2,3) ||substr(CIDADE,2,2) ||SUBSTR(CNPJ,3,2) "Nova Senha" from CLIENTE;
