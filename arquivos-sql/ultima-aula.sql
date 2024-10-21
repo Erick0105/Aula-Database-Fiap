58b520f2aea0c63516f8c1d38f244cbc89c258d7
Erick0105 <erickaxs0105@gmail.com>
2024-10-21 11:38:20 -0300

    inner, left e right join

--- arquivos-sql/ultima-aula.sql
+++ arquivos-sql/ultima-aula.sql
@@ -17,6 +17,11 @@ SELECT cod_ven, COUNT(NUM_PEDIDO) from PEDIDO group by COD_VEN order by 2;
 SELECT NOME_VEN, SALARIO_FIXO from VENDEDOR
 where SALARIO_FIXO in (SELECT max(SALARIO_FIXO) from VENDEDOR);
 
+--Exibir o nome do vendedor que tem o maior e menor salários
+SELECT NOME_VEN, SALARIO_FIXO from VENDEDOR where SALARIO_FIXO in 
+(SELECT max(SALARIO_FIXO) from VENDEDOR, SELECT min(SALARIO_FIXO) from VENDEDOR);
+
+SELECT * from CLIENTE where COD_CLIE IN(870,110,830,157);
 
 --Funções caracteres
 
@@ -33,3 +38,42 @@ SELECT NOME_CLIE, SUBSTR(NOME_CLIE,2,3) from CLIENTE;
 
 --nova senha
 SELECT SUBSTR(CEP,2,3) ||substr(CIDADE,2,2) ||SUBSTR(CNPJ,3,2) "Nova Senha" from CLIENTE;
+
+-- Junção de tabelas
+
+/*
+Inner join = igualdade
+left join = igualdade e diferença a esquerda
+right join = igualdade e diferença a direita
+*/
+
+drop table cargo CASCADE CONSTRAINTs;
+drop TABLE FUNCIONARIO CASCADE CONSTRAINTS;
+
+create table cargo (id_cargo number(2) primary key,
+                    nm_cargo varchar(20) not null unique,
+                    salario number(10,2));
+insert into cargo values (1,'DBA', 25000);
+insert into cargo values (2,'prog Java', 20000);
+insert into cargo values (3,'Analista de Sist', 20000);
+insert into cargo values (4,'Estágio', 6000);
+commit;
+
+create table FUNCIONARIO (id_fun number(2) PRIMARY KEY,
+                            nm_fun VARCHAR(20) not NULL,
+                            FK_CARGO REFERENCES cargo);
+
+insert into FUNCIONARIO values (1, 'Marcel', 1);
+insert into FUNCIONARIO values (2, 'Roberta', 1);
+insert into FUNCIONARIO values (3, 'Juliano', 2);
+insert into FUNCIONARIO values (4, 'Carmem', 3);
+insert into FUNCIONARIO values (5, 'Ricardo', NULL);
+commit;
+
+--Aqui é só a igualdade
+SELECT nm_fun, NM_CARGO from FUNCIONARIO Inner join cargo on id_cargo = FK_CARGO;
+
+--Aqui é o lado esquerdo mais a igualdade
+SELECT nm_fun, NM_CARGO from FUNCIONARIO left join cargo on id_cargo = FK_CARGO;
+--Aqui é o lado direito mais a igualdade
+SELECT nm_fun, NM_CARGO from FUNCIONARIO right join cargo on id_cargo = FK_CARGO;
